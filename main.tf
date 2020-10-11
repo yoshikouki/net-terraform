56eb1e2ed1a43defb4892992c5199cb5cd52ea5e
yoshikouki <yoshikouki@gmail.com>
2020-10-11 11:16:56 +0900

    記述方法を現在のバージョンに合わせる

--- main.tf
+++ main.tf
@@ -2,15 +2,26 @@ variable "access_key" {}
 variable "secret_key" {}
 variable "region" {}
 
-provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.sevret_key}"
-  region = "${var.region}"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.10"
+    }
+  }
 }
 
+# Configure the AWS Provider
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = var.region
+}
+
+# Create a VPC
 resource "aws_vpc" "vpc-1" {
   cidr_block = "10.0.0.0/16"
-  tags {
+  tags = {
     Name = "vpc-1"
   }
 }
